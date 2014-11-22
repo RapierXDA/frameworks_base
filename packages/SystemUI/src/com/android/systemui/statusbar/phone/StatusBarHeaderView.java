f0bf4124a2d5ac8570b21a89577d70a376569f85
MrBaNkS <banksmi09@gmail.com>
2014-11-22 07:22:21 -0600

    Add option for screenshot in powermenu and fix long press Conflicts: 	core/res/res/values/arrays.xml 	core/res/res/values/strings.xml 	core/res/res/values/symbols.xml 	policy/src/com/android/internal/policy/impl/GlobalActions.java
    Conflicts:
    	core/res/res/values/broken_arrays.xml
    	core/res/res/values/broken_strings.xml
    	core/res/res/values/broken_symbols.xml
    	core/res/res/values/strings.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/res/res/values/custom_symbols.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/res/res/values/custom_symbols.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -66,7 +66,7 @@ import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 /**
  * The view to manage the header area in the expanded status bar.
  */
-public class StatusBarHeaderView extends RelativeLayout implements View.OnClickListener,
+public class StatusBarHeaderView extends RelativeLayout implements View.OnClickListener, View.OnLongClickListener,
         BatteryController.BatteryStateChangeCallback, NextAlarmController.NextAlarmChangeCallback,
         WeatherController.Callback {
 
